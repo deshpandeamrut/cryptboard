8873b13f6f93c506ce7e5f96a296291faabf03d8
Amrut <XKS9@eurofins.com>
2019-05-14 20:20:30 +0530

    Cleanup

--- cryptboard - Copy/makeCurlCall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while [ true ]; do 
-  if [ $(expr $(date +%s) % 60) -eq 0 ]; then 
-    echo "top o da minute";
-    curl http://nammabagalkot.in/cryptboard/createData.php
-  fi; 
-  sleep 5; 
-done
--- cryptboard - Copy/runPhpScript.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-
-while(true){
-	file_get_contents("createData.php");
-	sleep(5);
-}
-
-?>
